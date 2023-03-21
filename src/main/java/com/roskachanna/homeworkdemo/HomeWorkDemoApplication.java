6d1d91789f836f8b00a95e23500cdfa43ab006bc
annaroskach <danuta_tyt@mail.ru>
2023-03-21 21:52:01 +0300

    homework17

---
+++ src/main/java/com/roskachanna/homeworkdemo/HomeWorkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.roskachanna.homeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkDemoApplication.class, args);
+	}
+
+}
