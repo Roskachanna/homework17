7a6d31ac10b4f2b125bb5042ff87ca7a4f88e9c6
annaroskach <danuta_tyt@mail.ru>
2023-03-21 21:52:01 +0300

    homework 17

---
+++ src/test/java/com/roskachanna/homeworkdemo/HomeWorkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roskachanna.homeworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
