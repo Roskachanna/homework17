87ae4d0e294e28391d4e666cd3fd3c319b836cde
annaroskach <danuta_tyt@mail.ru>
2023-03-24 21:43:14 +0300

    homework17

--- src/main/java/com/roskachanna/homeworkdemo/controller/CalculatorController.java
+++ src/main/java/com/roskachanna/homeworkdemo/controller/CalculatorController.java
@@ -1,12 +1,51 @@
 package com.roskachanna.homeworkdemo.controller;
 
+import com.roskachanna.homeworkdemo.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
-    @GetMapping
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping("/")
     public String greeting(){
         return "Добро пожаловать в калькулятор";
     }
+    @GetMapping("/plus ")
+    public Integer plus(
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ){
+        return calculatorService.plus(num1, num2);
+    }
+    @GetMapping("/minus ")
+    public Integer minus(
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ){
+        return calculatorService.minus(num1, num2);
+    }
+    @GetMapping("/multiply ")
+    public Integer multiply(
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ){
+        return calculatorService.multiply(num1, num2);
+    }
+    @GetMapping("/divide ")
+    public Integer divide(
+            @RequestParam("num1") Integer num1,
+            @RequestParam("num2") Integer num2
+    ){
+        return calculatorService.divide(num1, num2);
+    }
+
 }
---
+++ src/main/java/com/roskachanna/homeworkdemo/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,27 @@
+package com.roskachanna.homeworkdemo.service.impl;
+
+import com.roskachanna.homeworkdemo.service.CalculatorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public Integer plus(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public Integer minus(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public Integer multiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public Integer divide(Integer num1, Integer num2) {
+        return num1 / num2;
+    }
+}
